f7db98bc16c041ecd8e342dd97d1753698365f71
John Faulkner <razar1@sbcglobal.net>
2017-02-11 06:39:01 -0800

    Added autonomous states
    created left, middle, and right autonomous modes

--- AutonomousStates.py
+++ AutonomousStates.py
@@ -8,6 +8,58 @@ from wpilib import RobotDrive
 from Utilities import UtilityFunctions
 
 class AutoStates():
+    gearPlaced = False
+    step = ""
     
-    def simpleDeliverGear(MyRobot):
-        pass
+    def left(MyRobot, placeGear):
+        if placeGear:
+            #navigate to the gear peg
+        else:
+            #drive straight
+
+    """
+    placeGear is a boolean, crossOn is a string (left, right)
+    """
+    def middle(MyRobot, placeGear, crossOn):
+        if step == "":
+            if placeGear:
+                step = "lineUp"
+            elif crossOn != "":
+                step = crossOn
+
+        if step == "lineUp":
+            #Spin robot to line up to goal
+        if step == "go":
+            #driveNumSeconds to the peg
+            #At the end here set step = to 'leftFromPeg' or 'rightFromPeg'
+        if step == "leftFromPeg"
+            #cross the line on the left from the peg
+        if step == "rightFromPeg"
+            #cross the line on the right from the peg
+        if step == "left":
+            #cross the line on the left
+        if step == "right"
+            #cross the line on the right
+            
+    def right(MyRobot, placeGear, shootBalls):
+        if step == "":
+            if shootBalls:
+                step = "shoot"
+            else:
+                step = "lineUp"
+                
+        if step == "shoot"
+            #Shoot all of balls
+        if step == "lineUp":
+            #Spin robot to line up to goal
+        if step == "go":
+            #driveNumSeconds to the peg
+            #At the end here set step = to 'leftFromPeg' or 'rightFromPeg'
+        if step == "leftFromPeg"
+            #cross the line on the left from the peg
+        if step == "rightFromPeg"
+            #cross the line on the right from the peg
+        if step == "left":
+            #cross the line on the left
+        if step == "right"
+            #cross the line on the right
